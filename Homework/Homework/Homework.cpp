c2357b8b92d62904c7d5ca00c8b0a104aeea3f35
Mager2112 <92055047+Mager2112@users.noreply.github.com>
2023-09-26 20:48:58 +0300

    Add files via upload

---
+++ Homework/Homework/Homework.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <list>
+//HomeWork2
+///////1) Sub Array sum
+void Sub_Array(int arr[], int n, int m)
+{
+	int h = m;
+	int q = 0;
+	int sum = 0;
+	while (q < n)
+	{
+		for (int l = 0; l < n; l += m)
+		{
+			for (int i = l; i < h; i++)
+			{
+				if (i < n)
+				{
+					sum += arr[i];
+					q++;
+					
+				}
+			}
+			std::cout << "Sub Array #" << h / m << ". Sum is " << sum << std::endl;
+			sum = 0;
+			h += m;
+		}
+	}
+}
+
+int main()
+{
+	int n, m;
+	std::cin >> n >> m;
+	int* dinamich_array = new int[n];
+	for (int i = 0; i < n; i++)
+		std::cin >> dinamich_array[i];
+	Sub_Array(dinamich_array, n, m);
+}
